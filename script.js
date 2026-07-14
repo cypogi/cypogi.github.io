7bec0bb5fb894760b15c9371723db1e46c106d7b
Cy <112927914+cypogi@users.noreply.github.com>
2026-07-14 20:35:00 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//====================================
+// MELCHOR PORTFOLIO
+//====================================
+
+console.log("Portfolio Loaded!");
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    console.log("Welcome Melchor!");
+
+});
